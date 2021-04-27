06e3347c3a0f2aa925043ad63365f9bc0c5b89d0
changyonggang <changyonggang@bytedance.com>
2021-04-27 11:11:47 +0800

    feat: init project
    Change-Id: Ie10ea05e358e46eb3b39e612e3c1136bdd6c83a0

---
+++ cmake-build-debug/CMakeFiles/rocksdb_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rocksdb_demo.dir/main.cpp.o"
+  "rocksdb_demo"
+  "rocksdb_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rocksdb_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rocksdb_demo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rocksdb_demo.
+# This may be replaced when dependencies are built.
